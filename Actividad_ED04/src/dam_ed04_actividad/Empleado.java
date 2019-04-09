6eb2616e641b5997ce517fdc0b6246c5918619ee
profesorjacinto15 <profesor.jacinto15@gmail.com>
2019-04-10 00:22:40 +0100

    Commit 5: Getter y Setter

--- Actividad_ED04/src/dam_ed04_actividad/Empleado.java
+++ Actividad_ED04/src/dam_ed04_actividad/Empleado.java
@@ -4,5 +4,25 @@ public class Empleado {
 	String nombre;
 	String apellidos;
 	String dni;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
 
 }
