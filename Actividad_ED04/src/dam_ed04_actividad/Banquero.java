e9ed46ab5fbb7ed1beea634491d1abab384f9d10
profesorjacinto15 <profesor.jacinto15@gmail.com>
2019-04-10 00:28:09 +0100

    Commit 7: Setter y getters para la clase Banquero

--- Actividad_ED04/src/dam_ed04_actividad/Banquero.java
+++ Actividad_ED04/src/dam_ed04_actividad/Banquero.java
@@ -1,5 +1,19 @@
 package dam_ed04_actividad;
 
 public class Banquero {
+	String nombre;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	String DNI;
 
 }
